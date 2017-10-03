940755826782ba0d09be61e2882f159e4f6fc88f
chrisz24 <chriszikopoulos@gmail.com>
2017-10-02 22:45:33 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@ $client->messages->create(
     '+19176174152',
     array(
         // A Twilio phone number you purchased at twilio.com/console
-        'from' => '+18622256372',
+        'from' => '+19173490168',
         // the body of the text message you'd like to send
         'body' => 'Hello from Christos Zikopoulos CS 643 Fall 2017'
     )
