a9f5e59174d7093484123b33e31f5d61d6c2cf47
chrisz24 <chriszikopoulos@gmail.com>
2017-10-16 10:22:05 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -11,16 +11,20 @@
 <?php
 // Required if your environment does not handle autoloading
 require '../vendor/autoload.php';
-
-
-use Twilio\Twiml;
-
-$response = new Twiml;
-$body = $_REQUEST['Body'];
-
-if( $body == 'hello' ){
-    $response->message('Hi!');
-}else if( $body == 'bye' ){
-    $response->message('Goodbye');
-}
-print $response;
+// Use the REST API Client to make requests to the Twilio REST API
+use Twilio\Rest\Client;
+// Your Account SID and Auth Token from twilio.com/console
+$sid = 'ACdbdf59dcc984f9593d622c60e94f6ac5';
+$token = '51614e44f59b7e12dc94f21f0bb31d78';
+$client = new Client($sid, $token);
+// Use the client to do fun stuff like send text messages!
+$client->messages->create(
+    // the number you'd like to send the message to
+    '+‭19176174152‬',
+    array(
+        // A Twilio phone number you purchased at twilio.com/console
+        'from' => '+18622256372',
+        // the body of the text message you'd like to send
+        'body' => 'Hello from Christos Zikopoulos CS 643 Fall 2017'
+    )
+);
